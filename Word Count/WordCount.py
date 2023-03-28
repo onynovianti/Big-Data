9bb250e0dfcabee764e3a444e3c6781bbc672c06
onynovianti <95726641+onynovianti@users.noreply.github.com>
2023-03-28 07:38:14 +0700

    Delete WordCount.py

--- Word Count/WordCount.py
+++
@@ -1,8 +0,0 @@
-from operator import add
-lines = sc.textFile("/home/cloudera/Documents/cloudera-manager.html")
-counts = lines.flatMap(lambda x: x.split(' ')) \
-              .map(lambda x: (x, 1)) \
-              .reduceByKey(add)
-output = counts.collect()
-for (word, count) in output:
-    print("%s: %i" % (word, count))
